04d7450f46d8284ef44d6ac579da3fa6c253fec5
Kris Iyer <kris87us@gmail.com>
2021-07-07 23:49:40 -0700

    Readme update

--- src/learn/tree/BST_LevelOrderTraversal.java
+++ src/learn/tree/BST_LevelOrderTraversal.java
@@ -7,20 +7,20 @@ import java.util.Queue;
 import models.TreeNodeJava;
 
 public class BST_LevelOrderTraversal {
-    public static void main(String[] args) {
-        levelOrder(new TreeNodeJava(3, new TreeNodeJava(9), new TreeNodeJava(20, new TreeNodeJava(15), new TreeNodeJava(7))));
-    }
+    // public static void main(String[] args) {
+    //     levelOrder(new TreeNodeJava(3, new TreeNodeJava(9), new TreeNodeJava(20, new TreeNodeJava(15), new TreeNodeJava(7))));
+    // }
 
-    public static List<List<Integer>> levelOrder(TreeNodeJava root) {
-        Queue<Integer> queue = new Queue<>();
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        helper(root, queue, result);
-        return result;
-    }
+    // public static List<List<Integer>> levelOrder(TreeNodeJava root) {
+    //     Queue<Integer> queue = new Queue<>();
+    //     List<List<Integer>> result = new ArrayList<List<Integer>>();
+    //     helper(root, queue, result);
+    //     return result;
+    // }
     
-    private static void helper(TreeNodeJava root, Queue<Integer> queue, List<List<Integer>> result) {
-        if (root == null) return;
+    // private static void helper(TreeNodeJava root, Queue<Integer> queue, List<List<Integer>> result) {
+    //     if (root == null) return;
         
-        queue.
-    }
+    //     queue.
+    // }
 }
